979d38bc263471660bb01e291ce948240379e115
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-04-15 14:46:38 +0100

    Add basic database users page

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,12 +1,84 @@
+import Table from 'rc-table';
 import React from 'react';
+
+import Button from '../UI/Button/Button';
 
 import withAuth from '../../hoc/withAuth/withAuth';
 
+import classes from './Users.module.css';
+
+
 class Users extends React.Component {
   render() {
+    const columns = [
+      {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email'
+      },
+      {
+        title: 'Actions',
+        dataIndex: 'actions',
+        key: 'actions'
+      }
+    ];
+
+    const adminActions = (
+      <React.Fragment>
+        <Button buttonType="Danger">Revoke admin rights</Button>
+        <Button buttonType="Danger">Delete account</Button>
+      </React.Fragment>
+    );
+
+    const userActions = (
+      <React.Fragment>
+        <Button buttonType="Danger">Grant admin rights</Button>
+        <Button buttonType="Danger">Delete account</Button>
+      </React.Fragment>
+    );
+
+    const adminData = [
+      { email: 'Jack@test.com',
+        actions: adminActions,
+        key: '1'
+      },
+      { email: 'Rose@test.com',
+        actions: adminActions,
+        key: '2'
+      }
+    ];
+
+    const userData = [
+      { email: 'Claire@test.com',
+        actions: userActions,
+        key: '1'
+      },
+      { email: 'Anthony@test.com',
+        actions: userActions,
+        key: '2'
+      }
+    ];
+
     return (
       <React.Fragment >
-        <h2>Welcome to user management</h2>
+        <Button buttonType="Success">Invite user</Button>
+
+        <Table
+          className={classes.Table}
+          title={() => <h2 className={classes['Table-title']}>Admins</h2>}
+          columns={columns}
+          data={adminData}
+          prefixCls='Table'
+        />
+
+        <Table
+          className={classes.Table}
+          title={() => <h2 className={classes['Table-title']}>Other database users</h2>}
+          columns={columns}
+          data={userData}
+          prefixCls='Table'
+        />
+
       </React.Fragment>
     );
   }
