06664d2e45ed7ea54279bda4b9a3e46e1b40843d
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-04-15 12:11:06 +0100

    Delete CrossMark component

--- src/components/UI/CrossMark/CrossMark.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from './CrossMark.module.css';
-
-const crossMark = ({ clicked }) =>
-  <span className={classes.CrossMark} onClick={clicked}></span>
-
-
-export default crossMark;
