a7bef965b3887d60d16697986e51da9d46d60feb
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-04-17 12:36:53 +0100

    Make logo local

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,6 +3,7 @@ import React from 'react';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 import classes from './Toolbar.module.css';
+import imgLogo from '../../../assets/logo-temp.png';
 
 const toolbar = (props) => {
   console.log(props);
@@ -21,7 +22,7 @@ const toolbar = (props) => {
       <div className={classes.Logo}>
         <img
           alt="Arcknight monsters"
-          src="https://imgplaceholder.com/250x50/transparent/0b0a0a?text=Arcknight+Monsters&font-size=30"
+          src={imgLogo}
         />
       </div>
       {nav}
