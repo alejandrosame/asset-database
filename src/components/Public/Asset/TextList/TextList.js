6cbd2c6938e6ec372bcbab730b5d6c8f28a56b4a
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-04-24 20:23:54 +0100

    Add tag filtering on public page

--- src/components/Public/Asset/TextList/TextList.js
+++ src/components/Public/Asset/TextList/TextList.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 import classes from './TextList.module.css';
 
-const textList = ({list, clicked}) =>
-  list.map(element =>
-    <span className={clicked ? classes.Tag : null}
-      key={element}
-      onClick={() => clicked ? clicked(element) : null}
-    >
-      {element}
-    </span>
+const textList = ({list, clicked, filtered}) =>
+  list.map(element =>{
+    let clsList = [];
+    if (clicked) clsList.push(classes.Tag);
+    if (filtered && filtered.has(element)) clsList.push(classes.TagFiltered);
+
+    return (
+      <span className={clsList.join(' ')}
+            key={element}
+            onClick={() => clicked ? clicked(element) : null}
+      >
+        {element}
+      </span>
+    )}
   )
 
 
--- src/containers/Public/Assets/Assets.js
+++ src/containers/Public/Assets/Assets.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import AdvancedTable from '../../../hoc/AdvancedTable/AdvancedTable';
 import assetMapper from './Asset/AssetMapper';
+import Tag from '../../../components/UI/Tag/Tag';
+
+import classes from './Assets.module.css';
 
 import assetsData from '../../../assets/data/assets.json';
 
@@ -24,10 +27,19 @@ const applySetError = (prevState) => ({
   isLoading: false,
 });
 
+const intersects = (array, set) => {
+  for (let val of array){
+    if (set.has(val)) return true;
+  }
+  return false;
+}
+
 class Assets extends React.Component {
   state = {
     hits: [],
     page: null,
+    productsFilter: new Set(),
+    tagsFilter: new Set(),
     isError: false,
     isLoading: false,
   };
@@ -68,18 +80,88 @@ class Assets extends React.Component {
       ? this.setState(applySetResult(result))
       : this.setState(applyUpdateResult(result));
 
+  onClickProduct = (value) => {
+    const productsFilter = new Set(this.state.productsFilter);
+    productsFilter.add(value);
+    this.setState({ productsFilter: productsFilter })
+  }
+
+  onClickTag = (value) => {
+    const tagsFilter = new Set(this.state.tagsFilter);
+    tagsFilter.add(value);
+    this.setState({ tagsFilter: tagsFilter })
+  }
+
+  onDeleteProduct = (value) => {
+    const productsFilter = new Set(this.state.productsFilter);
+    productsFilter.delete(value);
+    this.setState({ productsFilter: productsFilter })
+  }
+
+  onDeleteTag = (value) => {
+    const tagsFilter = new Set(this.state.tagsFilter);
+    tagsFilter.delete(value);
+    this.setState({ tagsFilter: tagsFilter })
+  }
+
+  filterHits = (hits) => {
+    let filteredHits = hits;
+
+    if (this.state.productsFilter.size > 0) {
+      filteredHits = filteredHits
+        .filter(f => this.state.productsFilter.has(f.product))
+    }
+    if (this.state.tagsFilter.size > 0) {
+      filteredHits = filteredHits
+        .filter(f => intersects(f.tags, this.state.tagsFilter))
+    }
+    return filteredHits;
+  }
 
   render() {
-    const mapper = assetMapper((el) => console.log(el), (el) => console.log(el));
+    const mapper = assetMapper(this.onClickProduct, this.onClickTag,
+                               this.state.productsFilter, this.state.tagsFilter);
+
+    let filterAnnouncement = null;
+    if (this.state.productsFilter.size + this.state.tagsFilter.size > 0){
+      const productTags = [...this.state.productsFilter].map(element =>
+        <Tag
+          key={element}
+          onDelete={() => this.onDeleteProduct(element)}
+        >
+          {element} (Product)
+        </Tag>
+      ).reduce( (arr, el) => {
+      return arr.concat(el);
+      }, [] )
+
+      const tags = [...this.state.tagsFilter].map(element =>
+        <Tag
+          key={element}
+          onDelete={() => this.onDeleteTag(element)}
+        >
+          {element}
+        </Tag>
+      ).reduce( (arr, el) => {
+      return arr.concat(el);
+      }, [] )
+
+      filterAnnouncement = <div className={classes.Announcement}>
+        <strong className={classes.AnnouncementText}>Currently only showing items that include:</strong> {productTags} {tags}
+      </div>
+    }
     return (
-      <AdvancedTable
-        list={mapper(this.state.hits)}
-        isError={this.state.isError}
-        isLoading={this.state.isLoading}
-        page={this.state.page}
-        onPaginatedSearch={this.onPaginatedSearch}
-        showHeader={false}
-      />
+      <React.Fragment>
+        {filterAnnouncement}
+        <AdvancedTable
+          list={mapper(this.filterHits(this.state.hits))}
+          isError={this.state.isError}
+          isLoading={this.state.isLoading}
+          page={this.state.page}
+          onPaginatedSearch={this.onPaginatedSearch}
+          showHeader={false}
+        />
+    </React.Fragment>
     );
   }
 }
